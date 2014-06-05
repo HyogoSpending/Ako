1fe12cafa2f9245ae765ab0e2f91e33667971842
HyogoSpending <HyogoSpending@gmail.com>
2014-06-05 17:20:05 +0900

    up

---
+++ js/setting.js
@@ -0,0 +1,43 @@
+/*
+ * Icons for COFOG taxonomy
+ *
+ */
+var Taxes = Taxes || {};
+
+Taxes.baseKoujo = 330000; // 住民税基礎控除
+Taxes.huyoKoujo = 330000; // 一人分の扶養控除
+Taxes.taxRate = 0.06; // 住民税率
+
+var OpenSpending = OpenSpending || {};
+
+OpenSpending.identifier = 'ako_2012_kessan';
+OpenSpending.year = '2012';
+
+OpenSpending.Styles = OpenSpending.Styles || {};
+
+OpenSpending.Styles.Cofog = {
+
+  /* Akoshi cofog */
+  '20120100': { icon: 'icons/legislative.svg', color: '#C75746', bcolor: '#b20000' },
+  '20120101': { icon: 'icons/legislative.svg', color: '#C75746', bcolor: '#b20000' },
+  '20120200': { icon: 'icons/publicaffairs.svg', color: '#C75746', bcolor: '#b20000' },
+  '20120201': { icon: 'icons/publicaffairs.svg', color: '#C75746', bcolor: '#b20000' },
+  '20120300': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#b20000' },
+  '20120301': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#b20000' },
+  '20120400': { icon: 'icons/garbage.svg', color: '#C75746', bcolor: '#b20000' },
+  '20120401': { icon: 'icons/garbage.svg', color: '#C75746', bcolor: '#b20000' },
+  '20120500': { icon: 'icons/economy-tourism.svg', color: '#C75746', bcolor: '#b20000' },
+  '20120501': { icon: 'icons/economy-tourism.svg', color: '#C75746', bcolor: '#b20000' },
+  '20120600': { icon: 'icons/construction.svg', color: '#C75746', bcolor: '#b20000' },
+  '20120601': { icon: 'icons/construction.svg', color: '#C75746', bcolor: '#b20000' },
+  '20120700': { icon: 'icons/fire-brigade.svg', color: '#C75746', bcolor: '#b20000' },
+  '20120701': { icon: 'icons/fire-brigade.svg', color: '#C75746', bcolor: '#b20000' },
+  '20120800': { icon: 'icons/education.svg', color: '#C75746', bcolor: '#b20000' },
+  '20120801': { icon: 'icons/education.svg', color: '#C75746', bcolor: '#b20000' },
+  '20120900': { icon: 'icons/public-debt.svg', color: '#C75746', bcolor: '#b20000' },
+  '20120901': { icon: 'icons/public-debt.svg', color: '#C75746', bcolor: '#b20000' },
+  '20121000': { icon: 'icons/money.svg', color: '#C75746', bcolor: '#b20000' },
+  '20121001': { icon: 'icons/money.svg', color: '#C75746', bcolor: '#b20000' }
+};
+
+
